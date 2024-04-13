44816942418e309362e8a74dbece0be6b174b644
ved2805 <vedrecharla@gmail.com>
2024-04-13 12:19:07 +0530

    Refactoring hello after testing

---
+++ main/helloworld/hello.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("Chris", "English")) //fmt.print is side effect
+	//so better to seperate domain from side effect
+}
