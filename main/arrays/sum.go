4542aa7f522ec8d42f8b7abd35de732ab5668955
ved2805 <vedrecharla@gmail.com>
2024-04-13 12:37:02 +0530

    Passes sum TCs

---
+++ main/arrays/sum.go
@@ -0,0 +1,9 @@
+package arrays
+
+func Sum(nums [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += nums[i]
+	}
+	return sum
+}
---
+++ main/arrays/sum_test.go
@@ -0,0 +1,14 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	nums := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(nums)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, nums)
+	}
+}
